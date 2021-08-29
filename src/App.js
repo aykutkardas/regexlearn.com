6d0dc3ee9bcc1450c4b6d39a0908419dd73e56f0
Aykut Kardaş <aykutkrds@gmail.com>
2021-08-29 19:12:55 +0300

    Add `localization` and some dummy steps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import "./App.css";
-import { useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
 import { IntlProvider } from "react-intl";
+import { ToastContainer } from "react-toastify";
+import mousetrap from "mousetrap";
+import "mousetrap-global-bind";
+
 import localization from "./localization";
 import Step from "./components/Step";
+import Navigation from "./components/Navigation";
+
 import data from "./data.json";
 
 function App() {
-  const defaultLang = "en-us";
+  const defaultLang = "tr-tr";
   const [lang, setLang] = useState(defaultLang);
   const [step, setStep] = useState(0);
+
+  const prevStep = () => {
+    if (step > 0) {
+      setStep(step - 1);
+    }
+  };
+
+  const nextStep = () => {
+    if (step < data.length - 1) {
+      setStep(step + 1);
+    }
+  };
+
+  useEffect(() => {
+    mousetrap.bindGlobal(["ctrl+left", "command+left"], prevStep);
+    mousetrap.bindGlobal(["ctrl+right", "command+right"], nextStep);
+
+    return () =>
+      mousetrap.unbindGlobal([
+        "ctrl+left",
+        "ctrl+right",
+        "command+left",
+        "command+right",
+      ]);
+  }, [step]);
 
   return (
     <IntlProvider
@@ -18,6 +50,13 @@ function App() {
     >
       <div className="App">
         <Step data={data[step]} />
+        <Navigation
+          steps={data}
+          step={step}
+          prevStep={prevStep}
+          nextStep={nextStep}
+        />
+        <ToastContainer />
       </div>
     </IntlProvider>
   );
--- src/localization/index.js
+++ src/localization/index.js
@@ -1,7 +1,9 @@
 import enUS from "./en-us.json";
+import trTR from "./tr-tr.json";
 
 const messages = {
   "en-us": enUS,
+  "tr-tr": trTR,
 };
 
 export default messages;
