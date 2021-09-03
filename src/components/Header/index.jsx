918466eb69ca27c4523a24df78792bc87a4d434d
Aykut Kardaş <aykutkrds@gmail.com>
2021-09-03 23:16:22 +0300

    Add `rel` attribute to header links

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,10 +6,18 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-social">
-        <a href="https://github.com/aykutkardas/easy-regex" target="_blank">
+        <a
+          href="https://github.com/aykutkardas/easy-regex"
+          target="_blank"
+          rel="noreferrer"
+        >
           <Icon icon="github" size={20} color="white" />
         </a>
-        <a href="https://twitter.com/aykutkardas" target="_blank">
+        <a
+          href="https://twitter.com/aykutkardas"
+          target="_blank"
+          rel="noreferrer"
+        >
           <Icon icon="twitter" size={20} color="white" />
         </a>
       </div>
