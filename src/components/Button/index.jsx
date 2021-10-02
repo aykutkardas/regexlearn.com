238024f1bdf7ed5062ddd2bc028fa70d76ed5afd
Aykut Kardaş <aykutkrds@gmail.com>
2021-10-02 22:22:35 +0300

    Fix Button component end line warnings

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,7 @@
 import "./button.scss";
 
 const Button = ({ children, ...props }) => {
-  return (
-    <button {...props}>
-      {children}
-    </button>
-  );
+  return <button {...props}>{children}</button>;
 };
 
 export default Button;
