3d8e62ae5ca869d693f2234a5840a6e5e41f62aa
Aykut Kardaş <aykutkrds@gmail.com>
2021-10-09 15:40:38 +0300

    Fix header path of 404 page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,10 +1,10 @@
-import './not-found.scss';
+import "./not-found.scss";
 
 import { Link } from "react-router-dom";
 import { FormattedMessage } from "react-intl";
 
-import LandingHeader from "../../components/LandingHeader";
-import Button from "../../components/Button";
+import LandingHeader from "@pages/LandingPage/components/Header";
+import Button from "@components/Button";
 
 function NotFoundPage() {
   return (
@@ -12,13 +12,14 @@ function NotFoundPage() {
       <LandingHeader />
       <div className="container">
         <div className="content not-found">
-          <h1>
-            404
-          </h1>
+          <h1>404</h1>
           <p>
-            <FormattedMessage id="notFound.intro" values={{
-              br: <br />
-            }}/>
+            <FormattedMessage
+              id="notFound.intro"
+              values={{
+                br: <br />,
+              }}
+            />
           </p>
           <Link to="/learn">
             <Button className="warning">
@@ -28,7 +29,7 @@ function NotFoundPage() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
