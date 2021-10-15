049e98133df9bb91201ea309f18958bcc00cea9c
Aykut Kardaş <aykutkrds@gmail.com>
2021-10-16 00:24:55 +0300

    Split InteractiveArea component

--- src/pages/LearnPage/components/Step/index.jsx
+++ src/pages/LearnPage/components/Step/index.jsx
@@ -1,206 +1,42 @@
 import "./step.scss";
 
-import { useState, useEffect, useRef } from "react";
 import { useIntl } from "react-intl";
-import { toast } from "react-toastify";
-import cx from "classnames";
-import Mousetrap from "mousetrap";
-import _ from "lodash";
-
-import Hint from "@components/Hint";
-import FlagBox from "@components/FlagBox";
 
 import tagWrapper from "@utils/tagWrapper";
-import setCaretPosition from "@utils/setCaretPosition";
 
-import shortcuts from "../../../../shortcuts";
+import InteractiveArea from "../InteractiveArea";
 
 function Steps({ data, step, error: parentError, onChangeSuccess }) {
-  const [regex, setRegex] = useState(data.initialValue || "");
-  const [flags, setFlags] = useState(data.initialFlags || "");
-  const [isChanged, setIsChanged] = useState(false);
-  const [content, setContent] = useState(null);
-  const [error, setError] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const [match, setMatch] = useState(false);
-  const regexInput = useRef(null);
   const { formatMessage } = useIntl();
 
-  const focusInput = () => {
-    if (regexInput) {
-      regexInput.current.focus();
-    }
-  };
+  const title = tagWrapper(
+    formatMessage({ id: data.title }),
+    /`(\S*?[^`]*)`/gim,
+    "step-word"
+  ).replace(/\\n/gim, "<br/>");
 
-  const blurInput = () => {
-    if (regexInput) {
-      regexInput.current.blur();
-    }
-  };
+  const description = tagWrapper(
+    formatMessage({ id: data.description }),
+    /`(\S*?[^`]*)`/gim,
+    "step-word"
+  ).replace(/\\n/gim, "<br/>");
 
-  const onChange = (e) => {
-    setIsChanged(true);
-    setRegex(e.target.value);
-  };
-
-  const checkRegex = () => {
-    try {
-      let $regex = regex;
-      [...$regex.matchAll(/\\(\d+)/g)].forEach((item) => {
-        $regex = $regex.replace(
-          `\\${item[1]}`,
-          `\\${parseInt(item[1], 10) + 1}`
-        );
-      });
-
-      const reg = new RegExp(`(${$regex})`, flags);
-      const matchType = flags?.includes("g") ? "matchAll" : "match";
-      const regResult = [...data.content[matchType](reg)]
-        .map((res) => res[0])
-        .filter((res) => !!res);
-
-      const isMatch =
-        data.answer.length === regResult.length &&
-        _.isEmpty(_.xor(data.answer, regResult));
-
-      setMatch(isMatch);
-
-      const isSuccess =
-        isMatch &&
-        data.regex === regex &&
-        _.isEmpty(_.xor(data.flags.split(""), flags.split("")));
-
-      setSuccess(isSuccess);
-
-      toast.dismiss();
-
-      if (regex) {
-        setContent(tagWrapper(data.content, reg, "step-result-tag"));
-      } else {
-        setContent(data.content);
-      }
-
-      if (isChanged && isSuccess) {
-        toast.success(formatMessage({ id: "general.completedStep" }), {
-          theme: "colored",
-          autoClose: true,
-          position: "top-center",
-        });
-
-        setError(false);
-      } else if (isMatch) {
-        setError(false);
-      } else {
-        setError(true);
-      }
-    } catch (err) {
-      console.log(err);
-      setError(true);
-    }
-  };
-
-  useEffect(() => {
-    onChangeSuccess(success);
-  }, [success, onChangeSuccess]);
-
-  useEffect(() => {
-    Mousetrap.bindGlobal(shortcuts.focus, (e) => {
-      e.preventDefault();
-      focusInput();
-    });
-
-    return () => Mousetrap.unbindGlobal(shortcuts.focus);
-  }, []);
-
-  useEffect(() => {
-    setError(false);
-    setSuccess(false);
-
-    if (data.interactive === false) {
-      setSuccess(true);
-      return;
-    }
-
-    checkRegex();
-    setContent(data.content);
-    setFlags(data.initialFlags);
-    setRegex(data.initialValue || "");
-    setIsChanged(false);
-    blurInput();
-    setTimeout(() => {
-      setCaretPosition(regexInput.current, data.cursorPosition);
-      focusInput();
-    }, 300);
-  }, [step, data.cursorPosition]);
-
-  useEffect(checkRegex, [regex, flags]);
-
-  const description = formatMessage({ id: data.description });
+  const isInteractive = data.interactive !== false;
 
   return (
-    <div
-      className={cx("step", {
-        error,
-        success,
-        match,
-        parentError,
-      })}
-    >
-      <h2
-        className="step-title"
-        dangerouslySetInnerHTML={{
-          __html: tagWrapper(
-            formatMessage({ id: data.title }),
-            /`(\S*?[^`]*)`/gim,
-            "step-word"
-          ).replace(/\\n/gim, "<br/>"),
-        }}
-      />
+    <div className="step">
+      <h2 className="step-title" dangerouslySetInnerHTML={{ __html: title }} />
       <p
         className="step-description"
-        dangerouslySetInnerHTML={{
-          __html: tagWrapper(
-            description,
-            /`(\S*?[^`]*)`/gim,
-            "step-word"
-          ).replace(/\\n/gim, "<br/>"),
-        }}
+        dangerouslySetInnerHTML={{ __html: description }}
       />
-      {data.interactive !== false && (
-        <>
-          <div
-            className="step-block step-block-content"
-            data-title={formatMessage({ id: "general.text" })}
-            dangerouslySetInnerHTML={{
-              __html: (content || data.content).replace(/\n/gm, "<br />"),
-            }}
-          />
-          <div
-            className="step-block step-block-regex"
-            data-title={formatMessage({ id: "general.regex" })}
-          >
-            <Hint regex={data.regex} flags={data.flags} />
-            <div className="step-input" data-flags={flags}>
-              <input
-                ref={regexInput}
-                key={step}
-                type="text"
-                style={{ width: regex.length * 15 || 50 }}
-                readOnly={data.readOnly}
-                value={regex}
-                onChange={onChange}
-                placeholder={formatMessage({
-                  id: "general.regex",
-                }).toLowerCase()}
-                spellCheck={false}
-              />
-            </div>
-            {data.useFlagsControl && (
-              <FlagBox flags={flags} setFlags={setFlags} />
-            )}
-          </div>
-        </>
-      )}
+      <InteractiveArea
+        isShow={isInteractive}
+        data={data}
+        step={step}
+        parentError={parentError}
+        onChangeSuccess={onChangeSuccess}
+      />
     </div>
   );
 }
