5b8c8d27de1a66681f518db50366f5c15cfea822
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-11 20:35:57 +0300

    Add `analytics` scripts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -43,6 +43,14 @@ function MyApp({ Component, pageProps }) {
         <div className={cx("App", { desktop: isDesktop })}>
           <ToastContainer />
           <Head>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZNDXNCMG6E" />
+            <script dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-ZNDXNCMG6E');
+            `}} />
             <link rel="shortcut icon" href="/favicon.svg" />
             <meta name="description" content="Learn RegEx, step by step, zero to advanced. Playground and Cheat Sheet." />
           </Head>
