04530240d80abea7b5ba598528cc4e20a681d2f3
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-13 15:15:54 +0300

    Remove unused hotkeys utils

--- src/utils/hotkeys.js
+++
@@ -1,9 +0,0 @@
-import hotkeys from "hotkeys-js";
-
-hotkeys.filter = function (event) {
-    var tagName = (event.target || event.srcElement).tagName;
-    hotkeys.setScope(/^(INPUT|TEXTAREA|SELECT)$/.test(tagName) ? 'input' : 'other');
-    return true;
-}
-
-export default hotkeys;
