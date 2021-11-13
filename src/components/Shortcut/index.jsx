eeae8f48304dbaa5559d63b057bcdd6c79acea79
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-13 14:47:01 +0300

    Fix shortcut `alt` key on MacOS and Update style

--- src/components/Shortcut/index.jsx
+++ src/components/Shortcut/index.jsx
@@ -2,14 +2,13 @@ import * as styles from "./Shortcut.module.css";
 import useOS from "src/utils/useOS";
 
 const Shortcut = ({ command }) => {
-  const { isDesktop } = useOS();
+  const { isMacOS, isDesktop } = useOS();
 
   if (!isDesktop) return null;
 
   const readableCommand = command
     .replace(/\+/g, " + ")
-    .replace(/right/g, ">")
-    .replace(/left/g, "<")
+    .replace(/alt/g, isMacOS ? "option" : "alt")
     .toUpperCase();
 
   return <div className={styles.shortcut}>{readableCommand}</div>;
