27eed6f068ab219af487440ff354a140624a0a99
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-14 19:11:56 +0300

    Refactor Progress component

--- src/components/Progress/index.jsx
+++ src/components/Progress/index.jsx
@@ -1,16 +1,20 @@
 import { Line } from "rc-progress";
 
+import * as styles from "./Progress.module.css";
+
 const Progress = ({ steps, step }) => {
   const percent = (step / steps.length) * 100;
+
   return (
-    <div className="progress">
+    <div className={styles.Progress}>
       <Line
         percent={percent}
+        className={styles.ProgressLine}
         strokeWidth="4"
-        strokeColor="#5ff59b"
-        trailColor="#5b5b5b"
       />
-      <div className="progress-status">{`${step} / ${steps.length - 1}`}</div>
+      <div className={styles.ProgressStatus}>
+        {`${step} / ${steps.length - 1}`}
+      </div>
     </div>
   );
 };
