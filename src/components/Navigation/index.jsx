85d7572080d30d7c9a26c439018143e7e4a8ebd9
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-14 18:30:47 +0300

    Refactor Navigation component

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,6 +1,8 @@
 import { FormattedMessage } from "react-intl";
 import cx from "classnames";
 
+import * as styles from "./Navigation.module.css";
+
 import Icon from "../Icon";
 import Shortcut from "../Shortcut";
 
@@ -8,44 +10,45 @@ import shortcuts from "../../shortcuts";
 
 function Navigation({ steps, step, nextStep, prevStep, success, error }) {
   return !steps.length ? null : (
-    <div className="navigation">
+    <div className={styles.Navigation}>
       {step > 0 && (
         <div
-          className="navigation-step navigation-prev-step"
+          className={styles.NavigationStepPrev}
           role="button"
           tabIndex="0"
           onClick={prevStep}
           onKeyDown={prevStep}
         >
           <Shortcut command={shortcuts.prevStep} />
-          <div className="navigation-step-text">
-            <Icon className="navigation-icon" icon="arrow-left" size={20} />
-            <FormattedMessage id="general.prevStep" />
+          <div className={styles.NavigationStepText}>
+            <Icon className={styles.NavigationStepIcon} icon="arrow-left" size={20} />
+            <FormattedMessage id="general.prev" />
           </div>
         </div>
       )}
       {step < steps.length - 1 && (
         <div
-          className="navigation-step navigation-next-step"
+          className={styles.NavigationStepNext}
           role="button"
           tabIndex="0"
           onClick={nextStep}
           onKeyDown={nextStep}
         >
-          <Shortcut command={shortcuts.nextStep} />
-          <div className="navigation-step-text">
-            <FormattedMessage id="general.nextStep" />
-            <Icon className="navigation-icon" icon="arrow-right" size={20} />
+          <div className={styles.NavigationStepNextWrapper}>
+            <Shortcut command={shortcuts.nextStep} />
+            <Icon
+              className={cx(styles.NavigationStepStatusIcon, {
+                [styles.NavigationStepStatusIconUnlock]: success,
+                [styles.NavigationStepStatusIconError]: error,
+                "animate__animated animate__shakeX": error,
+              })}
+              icon={success ? "unlocked" : "lock"}
+            />
           </div>
-          <Icon
-            className={cx("status-icon", {
-              lock: !success,
-              unlock: success,
-              error,
-              "animate__animated animate__shakeX": error,
-            })}
-            icon={success ? "unlocked" : "lock"}
-          />
+          <div className={styles.NavigationStepText}>
+            <FormattedMessage id="general.next" />
+            <Icon className={styles.NavigationStepIcon} icon="arrow-right" size={20} />
+          </div>
         </div>
       )}
     </div>
