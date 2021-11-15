a0359f02f9a5cf59b8989de4ebe466d7ee1cc669
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-15 22:36:18 +0300

    Convert Footer scss to css module

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,17 +1,19 @@
 import Icon from "../Icon";
 
+import * as styles from "./Footer.module.css";
+
 function Footer() {
   return (
     <div className="container">
       <div className="row">
         <div className="col-sm-12">
-          <div className="landing-footer">
-            <div className="landing-footer-links">
+          <div className={styles.Footer}>
+            <div className={styles.FooterSocial}>
               <a
                 href="https://github.com/aykutkardas/regexlearn.com"
                 target="_blank"
                 rel="noreferrer"
-                className="landing-footer-link"
+                className={styles.FooterSocialLink}
               >
                 <Icon icon="github" size={20} color="white" />
               </a>
@@ -19,7 +21,7 @@ function Footer() {
                 href="https://twitter.com/aykutkardas"
                 target="_blank"
                 rel="noreferrer"
-                className="landing-footer-link"
+                className={styles.FooterSocialLink}
               >
                 <Icon icon="twitter" size={20} color="white" />
               </a>
