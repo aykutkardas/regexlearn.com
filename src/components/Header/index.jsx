0409a7f1101bf0da865fb2eb2e62d5a1bcffe32a
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-15 22:55:58 +0300

    Convert Header scss to css module

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,43 +3,48 @@ import ReactTooltip from "react-tooltip";
 import { FormattedMessage } from "react-intl";
 import Link from "next/link";
 import dynamic from "next/dynamic";
+import cx from "classnames";
+
+import * as styles from "./Header.module.css";
 
 import Icon from "../Icon";
+
 const LanguageSwitch  = dynamic(import("../LanguageSwitch"), { ssr: false });
 
 function LandingHeader() {
-
   return (
     <div className="container">
       <div className="row">
         <div className="col-sm-12">
-          <div className="landing-header">
+          <div className={styles.Header}>
             <Link href="/">
-              <a className="landing-header-brand">
+              <a className={styles.HeaderBrand}>
                 <Icon
-                  className="landing-header-brand-icon"
+                  className={cx(styles.HeaderBrandIcon, "visible-xs visible-sm")}
                   icon="unlocked"
                   removeInlineStyle
                 />
-                <span className="landing-header-brand-image">
-                  <img src="/logo.png" alt="RegexLearn" />
-                </span>
+                  <img
+                    className="visible-md visible-lg"
+                    src="/logo.png"
+                    alt="RegexLearn"
+                  />
               </a>
             </Link>
 
-            <div className="landing-header-links">
+            <div className={styles.HeaderNav}>
               <Link href="/learn" passHref>
-                <a className="landing-header-link">
+                <a className={styles.HeaderLink}>
                   <FormattedMessage id="landing.learn" />
                 </a>
               </Link>
               <Link href="/cheatsheet" passHref>
-                <a className="landing-header-link">
+                <a className={styles.HeaderLink}>
                   <FormattedMessage id="landing.cheatsheet" />
                 </a>
               </Link>
               <span
-                className="landing-header-link landing-link-disabled"
+                className={cx(styles.HeaderLink, styles.HeaderLinkDisabled)}
                 data-tip
                 data-for="coming-soon"
               >
@@ -49,7 +54,7 @@ function LandingHeader() {
                 <Icon
                   icon="lock"
                   size={16}
-                  className="landing-link-disabled-icon"
+                  className={styles.HeaderLinkDisabledIcon}
                 />
               </span>
               <ReactTooltip
