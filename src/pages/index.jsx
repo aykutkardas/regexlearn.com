9e685ed86274c81295252e337954fed63ec08c61
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-16 01:01:04 +0300

    Update landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,40 +19,46 @@ export default function Home() {
         <title>{formatMessage({ id: "meta.landing.title" })}</title>
       </Head>
       <div className="container">
-        <div className="row">
-          <div className="col-md-6 col-sm-12 landing">
-            <h1>
-              <FormattedMessage id="landing.title" />
-            </h1>
-            <p
-              dangerouslySetInnerHTML={{
-                __html: tagWrapper(
-                  formatMessage({ id: "landing.description" }),
-                  /`(\S*?[^`]*)`/gim,
-                  "highlight"
-                ),
-              }}
-            />
-            <Link href="/learn" passHref>
-              <a>
-                <Button className="success">
-                  <FormattedMessage id="general.startLearning" />
-                </Button>
-              </a>
-            </Link>
+        <div className="row main-section">
+          <div className="col-md-6 col-sm-12">
+            <div className="landing">
+              <h1>
+                <FormattedMessage id="landing.title" />
+              </h1>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: tagWrapper(
+                    formatMessage({ id: "landing.description" }),
+                    /`(\S*?[^`]*)`/gim,
+                    "highlight"
+                  ),
+                }}
+              />
+              <Link href="/learn" passHref>
+                <a>
+                  <Button className="success">
+                    <FormattedMessage id="general.startLearning" />
+                  </Button>
+                </a>
+              </Link>
+            </div>
           </div>
           <div className="col-md-6 col-sm-12 visible-md visible-lg intro">
-            <RegexDemo />
+            <img
+              src="/Done.webp"
+              width="100%"
+              alt="a relaxed man with his legs on the table"
+            />
           </div>
         </div>
       </div>
       <Footer />
     </>
-  )
+  );
 }
 
 export async function getStaticProps(context) {
   return {
     props: {},
-  }
+  };
 }
