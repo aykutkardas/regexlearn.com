57a0ccf07f31f6c07aeeb8419a948c7550e76806
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-16 13:57:44 +0300

    Add custom 404 page

---
+++ src/pages/404/index.jsx
@@ -0,0 +1,51 @@
+import Head from "next/head";
+import Link from "next/link";
+import { FormattedMessage, useIntl } from "react-intl";
+
+import * as styles from "./404.module.css";
+
+import Button from "../../components/Button";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+export default function Home() {
+  const { formatMessage } = useIntl();
+
+  return (
+    <>
+      <Header />
+      <Head>
+        <title>{formatMessage({ id: "meta.404.title" })}</title>
+      </Head>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className={styles.NotFound}>
+              <img className={styles.NotFoundImage} src="/404.webp" alt="404" />
+              <Link href="/" passHref>
+                <Button className="success">
+                  <FormattedMessage id="notFound.button" />
+                </Button>
+              </Link>
+              <p>
+                <FormattedMessage
+                  id="notFound.intro"
+                  values={{
+                    br: <br />,
+                  }}
+                />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export async function getStaticProps(context) {
+  return {
+    props: {},
+  };
+}
