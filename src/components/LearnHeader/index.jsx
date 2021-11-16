87b85ae23dd725f399a4174d3a1f3d7cbba37b47
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-16 12:36:04 +0300

    Convert LearnHeader scss to css module

--- src/components/LearnHeader/index.jsx
+++ src/components/LearnHeader/index.jsx
@@ -1,33 +1,32 @@
 import Link from "next/link";
+import cx from "classnames";
+
+import * as styles from "./LearnHeader.module.css";
 
 import Icon from "../Icon";
 import LanguageSwitch from "../LanguageSwitch";
 import Progress from "../Progress";
 
 const Header = ({ steps, step }) => (
-  <header className="header">
-    <div className="header-social">
-      <Link href="/" passHref>
-        <a><Icon icon="home" size={20} color="white" /></a>      
-      </Link>
-      <a
-        href="https://github.com/aykutkardas/regexlearn.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Icon icon="github" size={20} color="white" />
-      </a>
-      <a
-        href="https://twitter.com/aykutkardas"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Icon icon="twitter" size={20} color="white" />
-      </a>
-    </div>
-    <Progress steps={steps} step={step} />
-    <LanguageSwitch />
-  </header>
+  <div className={cx(styles.LearnHeader, "container-fluid")}>
+    <header className={styles.LearnHeaderRow}>
+      <div className={cx(styles.LearnHeaderHomeWrapper, "col-xs-4")}>
+        <div className="header-social">
+          <Link href="/" passHref>
+            <a>
+              <Icon icon="home" size={20} color="white" />
+            </a>
+          </Link>
+        </div>
+      </div>
+      <div className={cx(styles.LearnHeaderProgressWrapper, "col-xs-4")}>
+        <Progress steps={steps} step={step} />
+      </div>
+      <div className={cx(styles.LearnHeaderLanguageSwitchWrapper, "col-xs-4")}>
+        <LanguageSwitch />
+      </div>
+    </header>
+  </div>
 );
 
 export default Header;
