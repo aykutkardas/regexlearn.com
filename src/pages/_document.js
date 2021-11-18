2a3deb80fd62ba1785687767a60859329b797c13
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-19 00:06:55 +0300

    Disable analytics on development

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,29 +1,33 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 class Documents extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-ZNDXNCMG6E"
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+          {!isDevelopment && <>
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-ZNDXNCMG6E"
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag() {dataLayer.push(arguments); }
             gtag('js', new Date());
             gtag('config', 'G-ZNDXNCMG6E');
             `,
-            }}
-          />
-          <link rel="shortcut icon" href="/favicon.svg" />
-          <meta
-            name="description"
-            content="Learn RegEx, step by step, zero to advanced. Playground and Cheatsheet."
-          />
+              }}
+            />
+            <link rel="shortcut icon" href="/favicon.svg" />
+            <meta
+              name="description"
+              content="Learn RegEx, step by step, zero to advanced. Playground and Cheatsheet."
+            />
+          </>}
         </Head>
         <body>
           <Main />
