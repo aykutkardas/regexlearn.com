a40274fc66499a6f95357a4cd326e1772d047bde
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-20 02:24:32 +0300

    Update landing page

--- src/components/ProductButton/index.jsx
+++ src/components/ProductButton/index.jsx
@@ -1,6 +1,23 @@
-const ProductButton = () => {
-    return (<a href="https://www.producthunt.com/posts/regexlearn?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-regexlearn" target="_blank" rel="noreferrer"><img src={"https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=319999&theme=light?v="+new Date().getTime()} alt="RegexLearn - Learn RegEx step by step, from zero to advanced | Product Hunt" style={{width: 250, height: 54, marginBottom: 30}} width="250" height="54" /></a>
-    )
-}
+const ProductButton = props => {
+  return (
+    <a
+      href="https://www.producthunt.com/posts/regexlearn?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-regexlearn"
+      target="_blank"
+      rel="noreferrer"
+      {...props}
+    >
+      <img
+        src={
+          'https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=319999&theme=light?v=' +
+          new Date().getTime()
+        }
+        alt="RegexLearn - Learn RegEx step by step, from zero to advanced | Product Hunt"
+        style={{ width: 250, height: 54, marginBottom: 30 }}
+        width="250"
+        height="54"
+      />
+    </a>
+  );
+};
 
 export default ProductButton;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,17 @@
-import dynamic from "next/dynamic";
-import { FormattedMessage, useIntl } from "react-intl";
-import Head from "next/head";
-import cx from "classnames";
+import dynamic from 'next/dynamic';
+import { FormattedMessage, useIntl } from 'react-intl';
+import Head from 'next/head';
+import Link from 'next/link';
+import cx from 'classnames';
 
-import tagWrapper from "../utils/tagWrapper";
+import tagWrapper from '../utils/tagWrapper';
 
-const ProductButton = dynamic(import("../components/ProductButton"), { ssr: true });
-import Button from "../components/Button";
-import Icon from "../components/Icon";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Section from "../components/Section";
+const ProductButton = dynamic(import('../components/ProductButton'), { ssr: true });
+import Button from '../components/Button';
+import Icon from '../components/Icon';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Section from '../components/Section';
 
 export default function Home() {
   const { formatMessage } = useIntl();
@@ -19,35 +20,39 @@ export default function Home() {
     <>
       <Header />
       <Head>
-        <title>{formatMessage({ id: "meta.landing.title" })}</title>
+        <title>{formatMessage({ id: 'meta.landing.title' })}</title>
       </Head>
       <div className="container">
         <div className="row main-section">
-          <div className="col-md-6 col-sm-12">
+          <div className="col-xs-12 col-sm-12 col-md-6">
             <div className="landing">
-              <ProductButton />
               <h1>
                 <FormattedMessage id="landing.title" />
               </h1>
               <p
                 dangerouslySetInnerHTML={{
                   __html: tagWrapper(
-                    formatMessage({ id: "landing.description" }),
+                    formatMessage({ id: 'landing.description' }),
                     /`(\S*?[^`]*)`/gim,
-                    "highlight"
+                    'highlight',
                   ),
                 }}
               />
+              <Link href="/learn" passHref>
+                <a>
+                  <Button className={'success'}>
+                    <FormattedMessage id="general.startLearning" />
+                  </Button>
+                </a>
+              </Link>
             </div>
           </div>
-          <div className="col-md-6 col-sm-12 visible-md visible-lg intro">
-            <img
-              src="/Done.webp"
-              width="100%"
-              alt="a relaxed man with his legs on the table"
-            />
+          <div className="col-xs-12 col-sm-12 col-md-6 visible-md visible-lg intro">
+            <img src="/Done.webp" width="100%" alt="a relaxed man with his legs on the table" />
+            <ProductButton className="ph-button" />
           </div>
         </div>
+
         <Section
           title="section.learn.title"
           description="section.learn.content"
@@ -85,7 +90,7 @@ export default function Home() {
               target="_blank"
               rel="noreferrer"
             >
-              <Button className={cx("github", className)}>
+              <Button className={cx('github', className)}>
                 <Icon icon="github" size={16} color="white" />
                 <span>GitHub</span>
               </Button>
