a785c09a9fe7c2b5db0bec9caf8a500f1fc510e9
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-26 23:54:12 +0300

    Update color struct

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,37 +1,39 @@
-import { useState } from "react";
-import { FormattedMessage } from "react-intl";
+import { useState } from 'react';
+import { FormattedMessage } from 'react-intl';
 
-import CheatsheetDemo from "../CheatsheetDemo";
-import Icon from "../Icon";
+import CheatsheetDemo from '../CheatsheetDemo';
+import Icon from '../Icon';
 
-import * as styles from "./Collapse.module.css";
+import * as styles from './Collapse.module.css';
 
 function Collapse({ title, data }) {
   const [show, setShow] = useState(false);
 
   const toggle = () => {
     setShow(!show);
-  }
+  };
 
   return (
     <div className={styles.Collapse}>
-        <div className={styles.CollapseTitle} onClick={toggle}>
-            {title}
-            <Icon
-              className={styles.CollapseTitleIcon}
-              icon={show ? "caret-up" : "caret-down"}
-              style={{ color: show ? "var(--learn-white)" : "var(--learn-gray)"}}
-              size={12}
-            />
+      <div className={styles.CollapseTitle} onClick={toggle}>
+        {title}
+        <Icon
+          className={styles.CollapseTitleIcon}
+          icon={show ? 'caret-up' : 'caret-down'}
+          style={{ color: show ? 'var(--txt-1)' : 'var(--gray)' }}
+          size={12}
+        />
+      </div>
+      <div style={{ height: show ? 190 : 0 }} className={styles.CollapseContent}>
+        <div className={styles.CollapseContentInner}>
+          {data.description && (
+            <p className={styles.CollapseDescription}>
+              <FormattedMessage id={data.description} />
+            </p>
+          )}
+          <CheatsheetDemo data={data} />
         </div>
-        <div style={{ height: show ? 190 : 0}} className={styles.CollapseContent}>
-            <div className={styles.CollapseContentInner}>
-              {data.description && <p className={styles.CollapseDescription}>
-                <FormattedMessage id={data.description} />
-              </p>}
-              <CheatsheetDemo data={data} />
-            </div>
-        </div>
+      </div>
     </div>
   );
 }
