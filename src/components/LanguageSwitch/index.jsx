4b45144df3bcec4d781452ceaf82a02a2cc33f01
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-26 22:11:07 +0300

    Update style of LanguageSwitch

--- src/components/LanguageSwitch/index.jsx
+++ src/components/LanguageSwitch/index.jsx
@@ -1,27 +1,24 @@
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from 'react';
 
-import * as styles from "./LanguageSwitch.module.css";
-import Icon from "../Icon";
-import Shortcut from "../Shortcut";
+import * as styles from './LanguageSwitch.module.css';
+import Icon from '../Icon';
+import Shortcut from '../Shortcut';
 
-import langs, { langNames } from "../../localization";
-import shortcuts from "../../shortcuts";
-import { Context } from "../../contexts/LanguageContext";
-import Mousetrap from "../../utils/mousetrap";
+import langs, { langNames } from '../../localization';
+import shortcuts from '../../shortcuts';
+import { Context } from '../../contexts/LanguageContext';
+import Mousetrap from '../../utils/mousetrap';
 
-const langList = Object.keys(langs).map((langKey) => ({
+const langList = Object.keys(langs).map(langKey => ({
   value: langKey,
   label: langNames[langKey],
 }));
 
-const getCurrentIndex = (langList, lang) =>
-  langList.findIndex(({ value }) => value === lang);
+const getCurrentIndex = (langList, lang) => langList.findIndex(({ value }) => value === lang);
 
 const LanguageSwitch = () => {
   const { lang, setLang } = useContext(Context);
-  const [currentLangIndex, setCurrentLangIndex] = useState(
-    getCurrentIndex(langList, lang)
-  );
+  const [currentLangIndex, setCurrentLangIndex] = useState(getCurrentIndex(langList, lang));
 
   const toggleLang = useCallback(() => {
     const newLangIndex = currentLangIndex + 1;
@@ -42,20 +39,10 @@ const LanguageSwitch = () => {
 
   return (
     <div className={styles.LanguageSwitch}>
-      <div
-        className={styles.LanguageSwitchCurrent}
-        role="button"
-        tabIndex="0"
-        onClick={toggleLang}
-      >
+      <div className={styles.LanguageSwitchCurrent} role="button" tabIndex="0" onClick={toggleLang}>
         <Shortcut command={shortcuts.languageSwitch} />
         <span>{langNames[lang]}</span>
-        <Icon
-          className={styles.LanguageSwitchCurrentIcon}
-          icon="earth"
-          color="#fff"
-          size={20}
-        />
+        <Icon className={styles.LanguageSwitchCurrentIcon} icon="earth" color="#fff" size={16} />
       </div>
     </div>
   );
