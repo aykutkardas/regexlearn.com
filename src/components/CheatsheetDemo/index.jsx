d2913de47f59f8829a05fea77b507ea1c7927d0d
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-27 13:44:01 +0300

    Create `checkRegex` utils

--- src/components/CheatsheetDemo/index.jsx
+++ src/components/CheatsheetDemo/index.jsx
@@ -1,58 +1,47 @@
-import { useState, useEffect, useRef } from "react";
-import { useIntl } from "react-intl";
-import cx from "classnames";
+import { useState, useEffect } from 'react';
+import { useIntl } from 'react-intl';
+import cx from 'classnames';
 
-import * as styles from "./CheatsheetDemo.module.css";
+import * as styles from './CheatsheetDemo.module.css';
 
-import tagWrapper from "../../utils/tagWrapper";
+import checkRegex from '../../utils/checkRegex';
+import tagWrapper from 'src/utils/tagWrapper';
 
 function CheatsheetDemo({ data }) {
-  const [regex] = useState(data.regex || "");
+  const [regex] = useState(data.regex || '');
   const [content, setContent] = useState(null);
   const { formatMessage } = useIntl();
   const initialContent = data.content;
 
-  const checkRegex = () => {
-    try {
-      let $regex = regex;
-      [...$regex.matchAll(/\\(\d+)/g)].forEach((item) => {
-        $regex = $regex.replace(
-          `\\${item[1]}`,
-          `\\${parseInt(item[1], 10) + 1}`
-        );
-      });
+  const applyRegex = () => {
+    const { $regex } = checkRegex(data, { regex, flags: 'gmi' });
 
-      const reg = new RegExp(`(${$regex})`, "gmi");
-
-      if (regex) {
-        setContent(
-          tagWrapper(initialContent, reg, styles.CheatsheetDemoResultTag)
-        );
-      } else {
-        setContent(initialContent);
-      }
-    } catch (err) {
-      console.log(err);
+    if ($regex) {
+      setContent(tagWrapper(initialContent, $regex, styles.CheatsheetDemoResultTag));
+    } else {
+      setContent(initialContent);
     }
   };
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(checkRegex, []);
+  useEffect(applyRegex, []);
 
   return (
     <div className={styles.CheatsheetDemo}>
       <div
         className={cx(styles.CheatsheetDemoBlock, styles.CheatsheetDemoBlockContent)}
-        data-title={formatMessage({ id: "general.text" })}
+        data-title={formatMessage({ id: 'general.text' })}
         dangerouslySetInnerHTML={{
-          __html: (content || initialContent).replace(/\\n/gm, "<br />"),
+          __html: (content || initialContent).replace(/\\n/gm, '<br />'),
         }}
       />
       <div
         className={cx(styles.CheatsheetDemoBlock, styles.CheatsheetDemoBlockRegex)}
-        data-title={formatMessage({ id: "general.regex" })}
+        data-title={formatMessage({ id: 'general.regex' })}
       >
-        <span className={styles.CheatsheetDemoHiglightText} data-flags={data.flags}>{regex}</span>
+        <span className={styles.CheatsheetDemoHiglightText} data-flags={data.flags}>
+          {regex}
+        </span>
       </div>
     </div>
   );
--- src/components/InteractiveArea/index.jsx
+++ src/components/InteractiveArea/index.jsx
@@ -3,13 +3,13 @@ import { useState, useEffect, useRef } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { toast } from 'react-toastify';
 import cx from 'classnames';
-import _ from 'lodash';
 import lookie from 'lookie';
 
 import Mousetrap from '../../utils/mousetrap';
 import setCaretPosition from '../../utils/setCaretPosition';
 import tagWrapper from '../../utils/tagWrapper';
 import isSafari from '../../utils/isSafari';
+import checkRegex from '../../utils/checkRegex';
 
 import Hint from '../Hint';
 import ReportStep from '../ReportStep';
@@ -53,7 +53,7 @@ function InteractiveArea({ data, step, isShow, parentError, onChangeSuccess }) {
     setLocalStorage();
   };
 
-  const checkRegex = () => {
+  const applyRegex = () => {
     if (data.interactive === false) return true;
 
     if (isSafariAndAccept) {
@@ -69,34 +69,18 @@ function InteractiveArea({ data, step, isShow, parentError, onChangeSuccess }) {
       return true;
     }
 
-    try {
-      let $regex = regex;
-      [...$regex.matchAll(/\\(\d+)/g)].reverse().forEach(item => {
-        $regex = $regex.replace(`\\${item[1]}`, `\\${parseInt(item[1], 10) + 1}`);
-      });
+    const { isSuccess, isMatch, err, $regex } = checkRegex(data, { regex, flags });
 
-      const reg = new RegExp(`(${$regex})`, flags);
-      const matchType = flags?.includes('g') ? 'matchAll' : 'match';
-      const isMatchAll = matchType === 'matchAll';
-      const regResult = [...data.content[matchType](reg)]
-        .map(res => (isMatchAll ? res[0] : res))
-        .filter(res => !!res);
+    toast.dismiss();
 
-      const isMatch =
-        data.answer.length === regResult.length && _.isEmpty(_.xor(data.answer, regResult));
-
-      const isSuccess =
-        isMatch &&
-        data.regex.includes(regex) &&
-        _.isEmpty(_.xor(data.flags.split(''), flags.split('')));
-
+    if (err) {
+      setError(err);
+    } else {
       setMatch(isMatch);
       setSuccess(isSuccess);
 
-      toast.dismiss();
-
       if (regex) {
-        setContent(tagWrapper(data.content, reg, styles.InteractiveAreaResultTag));
+        setContent(tagWrapper(data.content, $regex, styles.InteractiveAreaResultTag));
       } else {
         setContent(data.content);
       }
@@ -110,9 +94,6 @@ function InteractiveArea({ data, step, isShow, parentError, onChangeSuccess }) {
       } else {
         setError(true);
       }
-    } catch (err) {
-      console.log(err);
-      setError(true);
     }
   };
 
@@ -147,7 +128,7 @@ function InteractiveArea({ data, step, isShow, parentError, onChangeSuccess }) {
     const progress = lookie.get('completedSteps') || [];
     const isCompletedStep = progress.includes(data.title);
 
-    checkRegex();
+    applyRegex();
     setContent(data.content);
     setFlags(isCompletedStep ? data.flags : data.initialFlags || '');
     setRegex((isCompletedStep ? data.regex[0] : data.initialValue) || '');
@@ -172,7 +153,7 @@ function InteractiveArea({ data, step, isShow, parentError, onChangeSuccess }) {
     return () => Mousetrap.unbind(shortcuts.focus);
   }, []);
 
-  useEffect(checkRegex, [regex, flags, step]);
+  useEffect(applyRegex, [regex, flags, step]);
 
   if (!isShow) return null;
 
