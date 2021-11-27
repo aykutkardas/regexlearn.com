3d1c02aa8028c60bfc4af1c3b1e780bce58847e4
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-27 12:05:00 +0300

    Fix style propblems of FlagBox

--- src/components/FlagBox/index.jsx
+++ src/components/FlagBox/index.jsx
@@ -1,62 +1,59 @@
-import { useCallback, useEffect, useMemo } from "react";
+import { useCallback, useEffect, useMemo } from 'react';
 
-import * as styles from "./FlagBox.module.css";
+import * as styles from './FlagBox.module.css';
 
-import Checkbox from "../Checkbox";
-import Shortcut from "../Shortcut";
+import Checkbox from '../Checkbox';
+import Shortcut from '../Shortcut';
 
-import Mousetrap from "src/utils/mousetrap";
-import tagWrapper from "src/utils/tagWrapper";
-import useOS from "src/utils/useOS";
-import shortcuts from "src/shortcuts";
+import Mousetrap from 'src/utils/mousetrap';
+import tagWrapper from 'src/utils/tagWrapper';
+import shortcuts from 'src/shortcuts';
 
 const FlagBox = ({ flags, setFlags, onChange }) => {
-  const { isDesktop } = useOS();
-
   const toggleFlag = useCallback(
-    (flag) => {
+    flag => {
       if (flags?.includes(flag)) {
-        setFlags(flags.replace(flag, ""));
+        setFlags(flags.replace(flag, ''));
       } else {
-        setFlags((flags || "") + flag);
+        setFlags((flags || '') + flag);
       }
       onChange(true);
     },
-    [flags, setFlags, onChange]
+    [flags, setFlags, onChange],
   );
 
   const handleClick = ({ target }) => {
-    const flag = target.getAttribute("flag");
+    const flag = target.getAttribute('flag');
     toggleFlag(flag);
   };
 
   const flagList = useMemo(
     () => [
       {
-        name: "global",
-        code: "g",
+        name: 'global',
+        code: 'g',
         command: shortcuts.flagGlobal,
         regex: /(g)/,
       },
       {
-        name: "multiline",
-        code: "m",
+        name: 'multiline',
+        code: 'm',
         command: shortcuts.flagMultiline,
         regex: /(m)/,
       },
       {
-        name: "case insensitive",
-        code: "i",
+        name: 'case insensitive',
+        code: 'i',
         command: shortcuts.flagCaseInsenstive,
         regex: /(i)/,
       },
     ],
-    []
+    [],
   );
 
   useEffect(() => {
-    flagList.forEach((flag) => {
-      Mousetrap.bindGlobal(flag.command, (e) => {
+    flagList.forEach(flag => {
+      Mousetrap.bindGlobal(flag.command, e => {
         e.preventDefault();
 
         toggleFlag(flag.code);
@@ -64,7 +61,7 @@ const FlagBox = ({ flags, setFlags, onChange }) => {
     });
 
     return () => {
-      flagList.forEach((flag) => {
+      flagList.forEach(flag => {
         Mousetrap.unbind(flag.code);
       });
     };
@@ -72,33 +69,30 @@ const FlagBox = ({ flags, setFlags, onChange }) => {
 
   const style = {};
 
-  if (isDesktop) {
-    style.height = 60;
-  }
-
   return (
-    <div className={styles.flagBox} style={style}>
+    <div className={styles.FlagBox}>
       {flagList.map(({ name, code, command, regex }) => (
-        <Checkbox
-          key={name}
-          id={`flag-${name}`}
-          flag={code}
-          type="checkbox"
-          checked={!!flags?.includes(code)}
-          onChange={handleClick}
-        >
-          <div className={styles.flagBoxItem}>
-            <span
-              id={`flag-${name}`}
-              dangerouslySetInnerHTML={{
-                __html: tagWrapper(name, regex, styles.flagBoxItemHighlight),
-              }}
-            />
-            <div className={styles.flagBoxShortcutWrapper}>
-              <Shortcut command={command} />
+        <div className={styles.FlagBoxItemWrapper} key={name}>
+          <Checkbox
+            id={`flag-${name}`}
+            flag={code}
+            type="checkbox"
+            checked={!!flags?.includes(code)}
+            onChange={handleClick}
+          >
+            <div className={styles.FlagBoxItem}>
+              <span
+                id={`flag-${name}`}
+                dangerouslySetInnerHTML={{
+                  __html: tagWrapper(name, regex, styles.FlagBoxItemHighlight),
+                }}
+              />
             </div>
+          </Checkbox>
+          <div className={styles.FlagBoxShortcutWrapper}>
+            <Shortcut command={command} />
           </div>
-        </Checkbox>
+        </div>
       ))}
     </div>
   );
