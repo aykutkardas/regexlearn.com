6f7262e915af6ab95f8086d80f881cb95b092559
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-27 12:40:14 +0300

    Convert styles of Checkbox to css module

--- src/components/ReportStep/index.jsx
+++ src/components/ReportStep/index.jsx
@@ -4,10 +4,12 @@ import { FormattedMessage } from 'react-intl';
 import Icon from '../Icon';
 import { Context } from '../../contexts/LanguageContext';
 
+import * as styles from './ReportStep.module.css';
+
 const ReportStep = ({ data, step }) => {
   const { lang } = useContext(Context);
 
-  const title = encodeURI('[Learn]: ');
+  const title = encodeURI('[Learn]: Type the title here...');
   const body = encodeURI(`
 **Step Number:** \`${step}\`
 **Step Name:** \`${data.title}\`
@@ -21,12 +23,12 @@ const ReportStep = ({ data, step }) => {
 **What is the problem you are experiencing?**
 
 
-    
 `);
 
   return (
-    <div className="report-step">
+    <div className={styles.ReportStep}>
       <a
+        className={styles.ReportStepLink}
         href={`https://github.com/aykutkardas/regexlearn.com/issues/new?title=${title}&body=${body}`}
         target="_blank"
         rel="noreferrer"
