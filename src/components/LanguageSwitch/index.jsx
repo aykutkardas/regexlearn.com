f72c960c69668176dc713f72a629ffee7d719bcd
Aykut Kardaş <aykutkrds@gmail.com>
2021-12-05 04:21:43 +0300

    Update LanguageSwicth

--- src/components/LanguageSwitch/index.jsx
+++ src/components/LanguageSwitch/index.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import * as styles from './LanguageSwitch.module.css';
 import Icon from 'src/components/Icon';
@@ -14,22 +15,15 @@ const langList = Object.keys(langs).map(langKey => ({
 }));
 
 const LanguageSwitch = () => {
-  const { lang, setLang } = useContext(Context);
+  const { lang } = useContext(Context);
   const [isOpen, setIsOpen] = useState(false);
-  const router = useRouter();
+  const { pathname } = useRouter();
 
   const toggleLang = () => {
     setIsOpen(!isOpen);
   };
 
-  const selectLang = (lang, e) => {
-    if (!isOpen) return;
-    e.preventDefault();
-
-    const href = getIntlPath(router.pathname, lang);
-    router.push(href);
-
-    // setLang(lang);
+  const selectLang = () => {
     setIsOpen(false);
   };
 
@@ -44,13 +38,11 @@ const LanguageSwitch = () => {
       {isOpen && (
         <div className={styles.LanguageSwitchList}>
           {getAvailableList().map(({ label, value }) => (
-            <div
-              className={styles.LanguageSwitchListItem}
-              key={value}
-              onClick={e => selectLang(value, e)}
-            >
-              <span>{label}</span>
-            </div>
+            <Link href={getIntlPath(pathname, value)} key={value}>
+              <a onClick={selectLang} className={styles.LanguageSwitchListItem}>
+                <span>{label}</span>
+              </a>
+            </Link>
           ))}
         </div>
       )}
