88a1df4dcbd10d7cba7602c0236b8eb628177da7
KeonWoo Ro <84064207+ro-ko@users.noreply.github.com>
2021-12-07 21:17:47 +0900

    Update index.js

--- src/localization/index.js
+++ src/localization/index.js
@@ -1,17 +1,20 @@
 import en from './en';
 import tr from './tr';
 import ru from './ru';
+import ko from './ko';
 
 const messages = {
   en,
   tr,
   ru,
+  ko,
 };
 
 export const langNames = {
   en: 'EN',
   tr: 'TR',
   ru: 'RU',
+  ko: 'KO',
 };
 
 export const defaultLocale = 'en';
