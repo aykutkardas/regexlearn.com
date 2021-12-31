5329436c8b64b21baf35c6d6add198ebb98b8c70
Aykut Kardaş <aykutkrds@gmail.com>
2021-12-31 12:18:52 +0300

    Update ProductHunt button

--- src/components/ProductButton/index.jsx
+++ src/components/ProductButton/index.jsx
@@ -1,16 +1,13 @@
 const ProductButton = props => {
   return (
     <a
-      href="https://www.producthunt.com/posts/regexlearn?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-regexlearn"
+      href="https://www.producthunt.com/posts/regexlearn?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-regexlearn"
       target="_blank"
       rel="noreferrer"
       {...props}
     >
       <img
-        src={
-          'https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=319999&theme=light?v=' +
-          new Date().getTime()
-        }
+        src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=319999&theme=light&period=daily"
         alt="RegexLearn - Learn RegEx step by step, from zero to advanced | Product Hunt"
         style={{ width: 250, height: 54, marginBottom: 30 }}
         width="250"
