551f4e88fc5b568d2108f08fa67b12d0de7dd184
Aykut Kardaş <aykutkrds@gmail.com>
2022-01-11 00:18:53 +0300

    Refactor Collapse

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
+import Collapsible from 'react-collapsible';
+import cx from 'classnames';
 
 import CheatsheetDemo from 'src/components/CheatsheetDemo';
 import Icon from 'src/components/Icon';
@@ -9,32 +11,31 @@ import * as styles from './Collapse.module.css';
 function Collapse({ title, data }) {
   const [show, setShow] = useState(false);
 
-  const toggle = () => {
-    setShow(!show);
-  };
-
   return (
-    <div className={styles.Collapse}>
-      <div className={styles.CollapseTitle} onClick={toggle}>
-        {title}
-        <Icon
-          className={styles.CollapseTitleIcon}
-          icon={show ? 'caret-up' : 'caret-down'}
-          style={{ color: show ? 'var(--txt-1)' : 'var(--gray)' }}
-          size={12}
-        />
-      </div>
-      <div style={{ height: show ? 190 : 0 }} className={styles.CollapseContent}>
-        <div className={styles.CollapseContentInner}>
-          {data.description && (
-            <p className={styles.CollapseDescription}>
-              <FormattedMessage id={data.description} />
-            </p>
-          )}
-          <CheatsheetDemo data={data} />
+    <Collapsible
+      transitionTime={300}
+      onOpening={() => setShow(true)}
+      onClosing={() => setShow(false)}
+      trigger={
+        <div className={styles.CollapseTitle}>
+          {title}
+          <Icon
+            className={cx(show ? styles.CollapseTitleIconActive : styles.CollapseTitleIcon)}
+            icon="caret-up"
+            size={12}
+          />
         </div>
+      }
+    >
+      <div className={styles.CollapseContent}>
+        {data.description && (
+          <p className={styles.CollapseDescription}>
+            <FormattedMessage id={data.description} />
+          </p>
+        )}
+        <CheatsheetDemo data={data} />
       </div>
-    </div>
+    </Collapsible>
   );
 }
 
