0a8b45207d752e1ca8c68d674f963abea496ca7a
Aykut Kardaş <aykutkrds@gmail.com>
2022-01-11 21:51:22 +0300

    Add open graph and twitter meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -26,6 +26,22 @@ class Documents extends Document {
             </>
           )}
           <link rel="shortcut icon" href="/favicon.svg" />
+
+          {/* <!-- Facebook Meta Tags --> */}
+          <meta property="og:url" content="https://regexlearn.com/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Regex Learn - Step by step, from zero to advanced." />
+          <meta property="og:description" content="Learn RegEx interactively, practice at your level, test and share your own RegEx." />
+          <meta property="og:image" content="https://regexlearn.com/images/og-regexlearn-image.jpg" />
+
+          {/* <!-- Twitter Meta Tags --> */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="regexlearn.com" />
+          <meta property="twitter:url" content="https://regexlearn.com/" />
+          <meta name="twitter:title" content="Regex Learn - Step by step, from zero to advanced." />
+          <meta name="twitter:description" content="Learn RegEx interactively, practice at your level, test and share your own RegEx." />
+          <meta name="twitter:image" content="https://regexlearn.com/images/og-regexlearn-image.jpg" />
+
         </Head>
         <body>
           <Main />
