7c1d88efc64b45e7d20ab43cbe5e9c169e7ae934
Aykut Kardaş <aykutkrds@gmail.com>
2022-01-12 20:28:34 +0300

    Create SeoTag component

---
+++ src/components/SeoTags/index.jsx
@@ -0,0 +1,31 @@
+const baseURL = 'https://regexlearn.com';
+
+const SeoTags = ({
+  title,
+  description,
+  href = '',
+  image = baseURL + '/images/og-regexlearn-image.jpg',
+}) => (
+  <>
+    {/* <!-- Common --> */}
+    <title>{title}</title>
+    <meta name="description" content={description} />
+
+    {/* <!-- Facebook Meta Tags --> */}
+    <meta property="og:url" content={baseURL + href} />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content={image} />
+
+    {/* <!-- Twitter Meta Tags --> */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="twitter:domain" content="regexlearn.com" />
+    <meta property="twitter:url" content={baseURL + href} />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+    <meta name="twitter:image" content={image} />
+  </>
+);
+
+export default SeoTags;
