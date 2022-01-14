4707d0d9f2e8061d0036d33627ab39f457b494d9
Aykut Kardaş <aykutkrds@gmail.com>
2022-01-14 17:12:54 +0300

    Update `/learn` page

--- src/pages/[lang]/learn/index.jsx
+++ src/pages/[lang]/learn/index.jsx
@@ -1,15 +1,20 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useIntl } from 'react-intl';
+import { useIntl, FormattedMessage } from 'react-intl';
+import cx from 'classnames';
 
 import SeoTags from 'src/components/SeoTags';
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
 import LessonBox from 'src/components/LessonBox';
 
+import tagWrapper from 'src/utils/tagWrapper';
+
 import { defaultLocale, locales } from 'src/localization';
 
 import lessons from 'src/data/lessons/index.json';
+
+import * as styles from './Learn.module.css';
 
 export default function Learn() {
   const { formatMessage } = useIntl();
@@ -28,6 +33,31 @@ export default function Learn() {
       </Head>
       <Header />
       <div className="container">
+        <div className={cx('row', styles.Section)}>
+          <div className={cx('col-xs-12 col-sm-12 col-md-8', styles.SectionContentWrapper)}>
+            <h2 className={styles.SectionTitle}>
+              <FormattedMessage id={'section.learn.title'} />
+            </h2>
+            <p
+              className={styles.SectionDescription}
+              dangerouslySetInnerHTML={{
+                __html: tagWrapper(
+                  formatMessage({ id: 'section.learn.content' }),
+                  /`(\S*?[^`]*)`/gim,
+                  styles.SectionHighlight,
+                ),
+              }}
+            />
+          </div>
+          <div className={cx('col-xs-12 col-sm-12 col-md-4', styles.SectionImageWrapper)}>
+            <img
+              src="/Learn.webp"
+              loading="lazy"
+              className={cx('img-responsive', styles.SectionImage)}
+              alt={formatMessage({ id: 'section.learn.imageAltText' })}
+            />
+          </div>
+        </div>
         <div className="row">
           {lessons.map(lesson => (
             <div key={lesson.key} className="col-xs-12 col-sm-4 col-md-3">
