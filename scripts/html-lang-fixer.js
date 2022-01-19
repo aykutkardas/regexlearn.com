08ed4ed4ad9c99704af707efbd6ba80653842a6f
Aykut Kardaş <aykutkrds@gmail.com>
2022-01-19 22:14:38 +0300

    Add `htmlLangFixer` script for SSG

---
+++ scripts/html-lang-fixer.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const BASE_PATH = './out';
+
+function replaceLangTag(path, locale) {
+  const content = fs.readFileSync(path, { encoding: 'utf-8' });
+  fs.writeFileSync(path, content.replace(/(<html lang=")en(">)/, `$1${locale}$2`));
+}
+
+function htmlLangFixer(path) {
+  const list = fs.readdirSync(path);
+
+  list.forEach(el => {
+    const currentPath = `${path}/${el}`;
+    const stats = fs.lstatSync(currentPath);
+
+    if (stats.isFile() && el.endsWith('.html')) {
+      const result = currentPath.match(/^\.\/out\/(en|es|tr|ru|zh-cn)\/?/);
+
+      if (result) {
+        replaceLangTag(currentPath, result[1]);
+      }
+    } else if (stats.isDirectory()) {
+      htmlLangFixer(currentPath);
+    }
+  })
+}
+
+
+htmlLangFixer(BASE_PATH);
