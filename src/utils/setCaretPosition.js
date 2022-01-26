998442b46197a02458ab541e39b6c834e4c13c15
Aykut Kardaş <aykutkrds@gmail.com>
2022-01-27 00:40:09 +0300

    Remove IE8 and below support for dynamic caret position

--- src/utils/setCaretPosition.js
+++ src/utils/setCaretPosition.js
@@ -1,19 +1,8 @@
 function setCaretPosition(inputEl, pos) {
-  if (!inputEl) return;
+  if (!inputEl || !inputEl.setSelectionRange) return;
 
-  // Modern browsers
-  if (inputEl.setSelectionRange) {
-    inputEl.focus();
-    inputEl.setSelectionRange(pos, pos);
-
-    // IE8 and below
-  } else if (inputEl.createTextRange) {
-    const range = inputEl.createTextRange();
-    range.collapse(true);
-    range.moveEnd("character", pos);
-    range.moveStart("character", pos);
-    range.select();
-  }
+  inputEl.focus();
+  inputEl.setSelectionRange(pos, pos);
 }
 
 export default setCaretPosition;
