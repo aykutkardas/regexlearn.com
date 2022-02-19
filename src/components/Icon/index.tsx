7ec3a6634f1bfb98f9e0c26f1ea46724beb51fad
Aykut Kardaş <aykutkrds@gmail.com>
2022-02-19 03:46:25 +0300

    Upgrade `react-icomoon`

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,16 +1,7 @@
-import IcoMoon from 'react-icomoon';
+import IcoMoon, { IconComponent } from 'react-icomoon';
 
 const iconSet = require('./selection.json');
 
-type IconArgs = {
-  icon: string;
-  size?: number | string;
-  color?: string;
-  className?: string;
-  onClick?: Function;
-  removeInlineStyle?: boolean;
-};
-
-const Icon = (props: IconArgs) => <IcoMoon iconSet={iconSet} {...props} />;
+const Icon: typeof IconComponent = props => <IcoMoon iconSet={iconSet} {...props} />;
 
 export default Icon;
