ec05c826739869d85c8eb1565a40b8f7e6e19e08
Aykut Kardaş <aykutkrds@gmail.com>
2022-04-15 00:24:33 +0300

    Upgrade modules (lookie, react-icomoon)

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,7 +1,7 @@
-import IcoMoon, { IconComponent } from 'react-icomoon';
+import IcoMoon, { IconProps } from 'react-icomoon';
 
 const iconSet = require('./selection.json');
 
-const Icon: typeof IconComponent = props => <IcoMoon iconSet={iconSet} {...props} />;
+const Icon = (props: IconProps) => <IcoMoon iconSet={iconSet} {...props} />;
 
 export default Icon;
