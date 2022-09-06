dd44524aab10274f32b169891fa60c35240ac088
Aykut Kardaş <aykutkrds@gmail.com>
2022-09-06 21:30:16 +0300

    Refactor (LanguageSelect)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import cx from 'classnames';
 import Icon from 'src/components/Icon';
 import Logo from 'src/components/Logo';
 import IntlLink from 'src/components/IntlLink';
-import LanguageSwitch from 'src/components/LanguageSwitch';
+import LanguageSelect from 'src/components/LanguageSelect';
 
 interface Props {
   isLearnPage?: boolean;
@@ -17,66 +17,56 @@ const Header = ({ isLearnPage }: Props) => (
         <Logo />
       </div>
       {isLearnPage && <div id="ProgressArea" className="flex justify-center flex-1" />}
-      <div className="flex-1">
-        <div className="flex items-center text-sm justify-end gap-2 sm:gap-4">
-          <IntlLink href="/[lang]/learn" passHref>
-            {({ isActive }) => (
-              <a
-                className={
-                  isActive ? 'text-regreen-400' : 'text-neutral-200 hover:text-regreen-400'
-                }
-              >
-                <FormattedMessage id="general.learn" />
-              </a>
-            )}
-          </IntlLink>
-          <IntlLink href="/[lang]/cheatsheet" passHref>
-            {({ isActive }) => (
-              <a
-                className={cx(
-                  isActive ? 'text-regreen-400' : 'text-neutral-200 hover:text-regreen-400',
-                  {
-                    'hidden md:block': isLearnPage,
-                  },
-                )}
-              >
-                <FormattedMessage id="general.cheatsheet" />
-              </a>
-            )}
-          </IntlLink>
-          <IntlLink href="/[lang]/playground" passHref>
-            {({ isActive }) => (
-              <a
-                className={cx(
-                  isActive ? 'text-regreen-400' : 'text-neutral-200 hover:text-regreen-400',
+      <div className="flex flex-1 items-center text-sm justify-end gap-2 sm:gap-4">
+        <IntlLink href="/[lang]/learn" passHref>
+          {({ isActive }) => (
+            <a
+              className={isActive ? 'text-regreen-400' : 'text-neutral-200 hover:text-regreen-400'}
+            >
+              <FormattedMessage id="general.learn" />
+            </a>
+          )}
+        </IntlLink>
+        <IntlLink href="/[lang]/cheatsheet" passHref>
+          {({ isActive }) => (
+            <a
+              className={cx(
+                isActive ? 'text-regreen-400' : 'text-neutral-200 hover:text-regreen-400',
+                { 'hidden md:block': isLearnPage },
+              )}
+            >
+              <FormattedMessage id="general.cheatsheet" />
+            </a>
+          )}
+        </IntlLink>
+        <IntlLink href="/[lang]/playground" passHref>
+          {({ isActive }) => (
+            <a
+              className={cx(
+                isActive ? 'text-regreen-400' : 'text-neutral-200 hover:text-regreen-400',
+                "relative after:right-0 after:content-['BETA'] after:text-[10px] after:text-regreen-400 after:absolute after:-top-3",
+                { 'hidden md:block': isLearnPage },
+              )}
+            >
+              <FormattedMessage id="general.playground" />
+            </a>
+          )}
+        </IntlLink>
 
-                  "relative after:right-0 after:content-['BETA'] after:text-[10px] after:text-regreen-400 after:absolute after:-top-3",
-                  {
-                    'hidden md:block': isLearnPage,
-                  },
-                )}
-                data-beta="true"
-              >
-                <FormattedMessage id="general.playground" />
-              </a>
-            )}
-          </IntlLink>
-
-          <a
-            href="https://github.com/aykutkardas/regexlearn.com"
-            target="_blank"
-            rel="noreferrer"
-            className={cx(
-              'text-neutral-200 hover:text-regreen-400',
-              'select-none relative items-baseline hidden justify-center',
-              isLearnPage ? 'lg:flex' : 'sm:flex',
-            )}
-          >
-            <span>GitHub</span>
-            <Icon icon="github" size={16} className="ml-1 w-4 h-4 text-neutral-200" />
-          </a>
-          <LanguageSwitch />
-        </div>
+        <a
+          href="https://github.com/aykutkardas/regexlearn.com"
+          target="_blank"
+          rel="noreferrer"
+          className={cx(
+            'text-neutral-200 hover:text-regreen-400',
+            'select-none relative items-baseline hidden justify-center',
+            isLearnPage ? 'lg:flex' : 'sm:flex',
+          )}
+        >
+          <span>GitHub</span>
+          <Icon icon="github" size={16} className="ml-1 w-4 h-4 text-neutral-200" />
+        </a>
+        <LanguageSelect />
       </div>
     </div>
   </header>
